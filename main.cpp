12e7c7bf1196318af6449d500b264f1a0f4542fd
Saiyam Jain <saiyamsandhir@gmail.com>
2023-11-03 20:57:49 +0530

    Add point object

--- main.cpp
+++ main.cpp
@@ -2,6 +2,42 @@
 #include <cmath>
 using namespace std;
 
+class Point
+{
+    public:
+        int x_coord, y_coord;
+
+        Point(int x = 0, int y = 0)
+        {
+            x_coord = x;
+            y_coord = y;
+        }
+
+        Point(Point& p)
+        {
+            x_coord = p.x_coord;
+            y_coord = p.y_coord;
+        }
+
+        double operator |(Point const& obj)
+        {
+            double distance;
+
+            distance = sqrt(pow(obj.x_coord - x_coord, 2) + pow(obj.y_coord - y_coord, 2));
+        
+            return distance;
+        }
+
+        Point operator ^(Point const& obj)
+        {
+            Point res;
+
+            res.x_coord = (x_coord + obj.x_coord) / 2;
+            res.y_coord = (y_coord + obj.y_coord) / 2;
+
+            return res;
+        }
+};
 class CartasianPlane
 {
     private:
@@ -45,13 +81,26 @@ class CartasianPlane
             }
         
         }
+
+        void plot(Point p)
+        {
+            plot(p.x_coord, p.y_coord);
+        }
 };
 
 int main()
 {
-    CartasianPlane cp(8, 7);
-    
-    cp.plot(3, 6);
+    CartasianPlane cp(3, 12);
+    Point a(1, 0), b(1, 10), c;
+
+    c = a^b;
+    cp.plot(a);
+    cout << endl;
+    cp.plot(b);
+    cout << endl;
+    cp.plot(c);
+    cout << endl;
+    cout << (a|b);
     
     return 0;
 }
